95fb04c541b3836dd2c46e283586ccf6d7c462d9
Nadezhda Brazhnikova <b.nadejda.v@mail.ru>
2024-07-10 19:34:57 +0300

    Добавляет задание 7.15 Отрисуй меня полностью: отображение фотографий других пользователей (#6)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-import {createPhotosDescription} from './data.js';
+//import {createPhotosDescription} from './data.js';
+import './photo-thumbnail.js';
 
 // eslint-disable-next-line no-console
-console.log(
+/*console.log(
   createPhotosDescription()
-);
+);*/
