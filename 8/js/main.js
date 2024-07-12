c6dc88de99829a7e204d65a239139aacdf84434d
Keks <keks@htmlacademy.ru>
2024-07-12 11:09:17 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,9 @@
+//import {createPhotosDescription} from './data.js';
+import './photo-thumbnail.js';
+import './photo-full.js';
+import './commenst.js';
+
+// eslint-disable-next-line no-console
+/*console.log(
+  createPhotosDescription()
+);*/
