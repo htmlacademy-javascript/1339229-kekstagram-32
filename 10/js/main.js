0628af0c546a0554eff17220baf080ad15d9d8bf
Keks <keks@htmlacademy.ru>
2024-07-25 12:20:45 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,10 @@
+//import {createPhotosDescription} from './data.js';
+import './photo-thumbnail.js';
+import './photo-full.js';
+import './commenst.js';
+import './form.js';
+
+// eslint-disable-next-line no-console
+/*console.log(
+  createPhotosDescription()
+);*/
